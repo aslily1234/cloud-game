d1820f44a16b30edce6a72b518cf4b032261672c
Sergey Stepanov <sergystepanov@gmail.com>
2020-06-28 15:51:58 +0300

    Add load/save test for the emulator

---
+++ pkg/emulator/libretro/nanoarch/nanoarch_test.go
@@ -0,0 +1,193 @@
+package nanoarch
+
+import (
+	"crypto/md5"
+	"fmt"
+	"github.com/giongto35/cloud-game/pkg/config"
+	"image"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+)
+
+// Emulator data mock.
+type EmulatorMock struct {
+	// Libretro compiled lib core name
+	core string
+
+	// hardcoded emulator channels
+	image chan GameFrame
+	audio chan []int16
+	input chan InputEvent
+
+	// selected emulator core meta
+	meta config.EmulatorMeta
+
+	// shared core paths (can't be changed)
+	paths EmulatorPaths
+}
+
+// Defines various emulator file paths.
+type EmulatorPaths struct {
+	assets string
+	cores  string
+	games  string
+}
+
+// Returns a properly stubbed emulator instance.
+// Be aware that it extensively uses global variables and
+// it should exist in one instance per a test run.
+// Don't forget to add at least one image channel consumer
+// since it will lock your thread otherwise.
+// Don't forget to close emulator mock with shutdownEmulator().
+func GetEmulatorMock(room string, system string) *EmulatorMock {
+	assetsPath := getAssetsPath()
+	metadata := config.EmulatorConfig[system]
+
+	// an emu
+	emu := &EmulatorMock{
+		core: path.Base(metadata.Path),
+
+		image: make(chan GameFrame, 30),
+		audio: make(chan []int16, 30),
+		input: make(chan InputEvent, 100),
+
+		meta: metadata,
+
+		paths: EmulatorPaths{
+			assets: cleanPath(assetsPath),
+			cores:  cleanPath(assetsPath + "emulator/libretro/cores/"),
+			games:  cleanPath(assetsPath + "games/"),
+		},
+	}
+
+	// global video output canvas
+	outputImg = image.NewRGBA(image.Rect(0, 0, emu.meta.Width, emu.meta.Height))
+
+	// global emulator instance
+	NAEmulator = &naEmulator{
+		meta:           emu.meta,
+		imageChannel:   emu.image,
+		audioChannel:   emu.audio,
+		inputChannel:   emu.input,
+		controllersMap: map[string][]controllerState{},
+		roomID:         room,
+		done:           make(chan struct{}, 1),
+		lock:           &sync.Mutex{},
+	}
+
+	return emu
+}
+
+// Returns initialized emulator mock with default params.
+// Spawns audio/image channels consumers.
+// Don't forget to close emulator mock with shutdownEmulator().
+func GetDefaultEmulatorMock(room string, system string, rom string) *EmulatorMock {
+	mock := GetEmulatorMock(room, system)
+	mock.loadRom(rom)
+	go mock.handleVideo(func(_ GameFrame) {})
+	go mock.handleAudio(func(_ []int16) {})
+
+	return mock
+}
+
+// Load a rom into the emulator.
+// The rom will be loaded from emulators' games path.
+func (emu EmulatorMock) loadRom(game string) {
+	fmt.Printf("%v %v\n", emu.paths.cores, emu.core)
+	coreLoad(emu.paths.cores+emu.core, false, false, "")
+	coreLoadGame(emu.paths.games + game)
+}
+
+// Close the emulator and cleanup its resources.
+func (emu EmulatorMock) shutdownEmulator() {
+	_ = os.Remove(getLatestSave())
+
+	close(emu.image)
+	close(emu.audio)
+	close(emu.input)
+
+	nanoarchShutdown()
+}
+
+// Emulate one frame with exclusive lock.
+func (emu EmulatorMock) emulateOneFrame() {
+	NAEmulator.GetLock()
+	nanoarchRun()
+	NAEmulator.ReleaseLock()
+}
+
+// Who needs generics anyway?
+// Custom handler for the video channel.
+func (emu EmulatorMock) handleVideo(handler func(image GameFrame)) {
+	for frame := range emu.image {
+		handler(frame)
+	}
+}
+
+// Custom handler for the audio channel.
+func (emu EmulatorMock) handleAudio(handler func(sample []int16)) {
+	for frame := range emu.audio {
+		handler(frame)
+	}
+}
+
+// Custom handler for the input channel.
+func (emu EmulatorMock) handleInput(handler func(event InputEvent)) {
+	for event := range emu.input {
+		handler(event)
+	}
+}
+
+// Returns the current emulator state and
+// the latest saved state for its session.
+// Locks the emulator.
+func (emu EmulatorMock) dumpState() (string, string) {
+	NAEmulator.GetLock()
+
+	state, _ := getState()
+	stateHash := getHash(state)
+	persistedStateHash := getSaveHash()
+
+	fmt.Printf("mem: %v, dat: %v\n", stateHash, persistedStateHash)
+
+	NAEmulator.ReleaseLock()
+	return stateHash, persistedStateHash
+}
+
+// Returns absolute path to the assets directory.
+func getAssetsPath() string {
+	appName := "cloud-game"
+	var (
+		// get app path at runtime
+		_, b, _, _ = runtime.Caller(0)
+		basePath   = filepath.Dir(strings.SplitAfter(b, appName)[0]) + "/" + appName + "/assets/"
+	)
+
+	return basePath
+}
+
+// Returns the full path to the emulator latest save.
+func getLatestSave() string {
+	return cleanPath(NAEmulator.GetHashPath())
+}
+
+// Returns latest save hash.
+func getSaveHash() string {
+	bytes, _ := ioutil.ReadFile(getLatestSave())
+	return getHash(bytes)
+}
+
+// Returns MD5 hash.
+func getHash(bytes []byte) string {
+	return fmt.Sprintf("%x", md5.Sum(bytes))
+}
+
+// Returns a proper file path for current OS.
+func cleanPath(path string) string {
+	return filepath.FromSlash(path)
+}
---
+++ pkg/emulator/libretro/nanoarch/savestates_test.go
@@ -0,0 +1,247 @@
+package nanoarch
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+type testRun struct {
+	room           string
+	system         string
+	rom            string
+	emulationTicks int
+}
+
+// Tests a successful emulator state save.
+func TestSave(t *testing.T) {
+	tests := []testRun{
+		{
+			room:           "test_save_ok_00",
+			system:         "gba",
+			rom:            "Sushi The Cat.gba",
+			emulationTicks: 100,
+		},
+		{
+			room:           "test_save_ok_01",
+			system:         "gba",
+			rom:            "anguna.gba",
+			emulationTicks: 10,
+		},
+	}
+
+	for _, test := range tests {
+		t.Logf("Testing [%v] save with [%v]\n", test.system, test.rom)
+
+		mock := GetDefaultEmulatorMock(test.room, test.system, test.rom)
+
+		for test.emulationTicks > 0 {
+			mock.emulateOneFrame()
+			test.emulationTicks--
+		}
+
+		fmt.Printf("[%-14v] ", "before save")
+		snapshot1, _ := mock.dumpState()
+		if err := NAEmulator.Save(); err != nil {
+			t.Errorf("Save fail %v", err)
+		}
+		fmt.Printf("[%-14v] ", "after  save")
+		snapshot1, snapshot2 := mock.dumpState()
+
+		if snapshot1 != snapshot2 {
+			t.Errorf("It seems rom state save has failed: %v != %v", snapshot1, snapshot2)
+		}
+
+		mock.shutdownEmulator()
+	}
+}
+
+// Tests save and restore function:
+//
+// Emulate n ticks.
+// Call save (a).
+// Emulate n ticks again.
+// Call load from the save (b).
+// Compare states (a) and (b), should be =.
+//
+func TestLoad(t *testing.T) {
+	tests := []testRun{
+		{
+			room:           "test_load_00",
+			system:         "nes",
+			rom:            "Super Mario Bros.nes",
+			emulationTicks: 100,
+		},
+		{
+			room:           "test_load_01",
+			system:         "gba",
+			rom:            "Sushi The Cat.gba",
+			emulationTicks: 1000,
+		},
+		{
+			room:           "test_load_02",
+			system:         "gba",
+			rom:            "anguna.gba",
+			emulationTicks: 100,
+		},
+	}
+
+	for _, test := range tests {
+		t.Logf("Testing [%v] load with [%v]\n", test.system, test.rom)
+
+		mock := GetDefaultEmulatorMock(test.room, test.system, test.rom)
+
+		fmt.Printf("[%-14v] ", "initial")
+		mock.dumpState()
+
+		for ticks := test.emulationTicks; ticks > 0; ticks-- {
+			mock.emulateOneFrame()
+		}
+		fmt.Printf("[%-14v] ", fmt.Sprintf("emulated %d", test.emulationTicks))
+		mock.dumpState()
+
+		if err := NAEmulator.Save(); err != nil {
+			t.Errorf("Save fail %v", err)
+		}
+		fmt.Printf("[%-14v] ", "saved")
+		snapshot1, _ := mock.dumpState()
+
+		for ticks := test.emulationTicks; ticks > 0; ticks-- {
+			mock.emulateOneFrame()
+		}
+		fmt.Printf("[%-14v] ", fmt.Sprintf("emulated %d", test.emulationTicks))
+		mock.dumpState()
+
+		if err := NAEmulator.Load(); err != nil {
+			t.Errorf("Load fail %v", err)
+		}
+		fmt.Printf("[%-14v] ", "restored")
+		snapshot2, _ := mock.dumpState()
+
+		if snapshot1 != snapshot2 {
+			t.Errorf("It seems rom state restore has failed: %v != %v", snapshot1, snapshot2)
+		}
+
+		mock.shutdownEmulator()
+	}
+}
+
+func TestStateConcurrency(t *testing.T) {
+	tests := []struct {
+		run testRun
+		// emulation frame cap
+		fps int
+		// determine random
+		seed int
+	}{
+		{
+			run: testRun{
+				room:           "test_concurrency_00",
+				system:         "gba",
+				rom:            "Sushi The Cat.gba",
+				emulationTicks: 120,
+			},
+			fps: 60,
+			seed: 42,
+		},
+		{
+			run: testRun{
+				room:           "test_concurrency_01",
+				system:         "gba",
+				rom:            "anguna.gba",
+				emulationTicks: 300,
+			},
+			fps: 60,
+			seed: 42 + 42,
+		},
+	}
+
+	for _, test := range tests {
+		t.Logf("Testing [%v] concurrency with [%v]\n", test.run.system, test.run.rom)
+
+		mock := GetEmulatorMock(test.run.room, test.run.system)
+		ops := &sync.WaitGroup{}
+		// quantum lock
+		qLock := &sync.Mutex{}
+		op := 0
+
+		mock.loadRom(test.run.rom)
+		go mock.handleVideo(func(frame GameFrame) {
+			if len(frame.Image.Pix) == 0 {
+				t.Errorf("It seems that rom video frame was empty, which is strange!")
+			}
+		})
+		go mock.handleAudio(func(_ []int16) {})
+		go mock.handleInput(func(_ InputEvent) {})
+
+		rand.Seed(int64(test.seed))
+		t.Logf("'Random' seed is [%v]\n", test.seed)
+		t.Logf("Save path is [%v]\n", getLatestSave())
+
+		//start := time.Now()
+		//elapsed := time.Since(start)
+		//t.Logf("Emulation of %v ticks has took %.2fs with %.2ffps\n",
+		//	test.run.emulationTicks, elapsed.Seconds(), float64(test.run.emulationTicks)/elapsed.Seconds())
+
+		_ = NAEmulator.Save()
+
+		// 60 fps emulation cap
+		ticker := time.NewTicker(time.Second / time.Duration(test.fps))
+
+		for range ticker.C {
+			select {
+			case <-NAEmulator.done:
+				mock.shutdownEmulator()
+				return
+			default:
+			}
+
+			op++
+			if op > test.run.emulationTicks {
+				NAEmulator.Close()
+			}
+
+			qLock.Lock()
+			mock.emulateOneFrame()
+			qLock.Unlock()
+
+			if lucky() && !lucky() {
+				ops.Add(1)
+				go func() {
+					qLock.Lock()
+					defer qLock.Unlock()
+
+					mock.dumpState()
+					// remove save to reproduce the bug
+					_ = NAEmulator.Save()
+					_, snapshot1 := mock.dumpState()
+					_ = NAEmulator.Load()
+					snapshot2, _ := mock.dumpState()
+
+					// Bug or feature?
+					// When you load a state from the file
+					// without immediate preceding save,
+					// it won't be in the loaded state
+					// even without calling retro_run.
+					// But if you pause the threads with a debugger
+					// and execute everything with steps, then it works.
+					// Possible background emulation?
+
+					if snapshot1 != snapshot2 {
+						t.Errorf("States are inconsistent %v != %v on tick %v\n", snapshot1, snapshot2, op)
+					}
+					ops.Done()
+				}()
+			}
+		}
+
+		ops.Wait()
+	}
+}
+
+// Returns random boolean.
+func lucky() bool {
+	return rand.Intn(2) == 1
+}
